748241aa9472d9e45ac4894f8abe2d8f5fd155f9
Xavier Redó <xavier.redo@marsbased.com>
2015-01-15 21:07:08 +0100

    first commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,7 @@
+//= require jquery.min
+//= require jquery.dropotron.min.js
+//= require jquery.scrolly.min.js
+//= require jquery.onvisible.min.js
+//= require skel.min.js
+//= require skel-layers.min.js
+//= require init.js
